cc6588738a6fd75deb1b35e1d8dfd82cb265f208
merja-shourov <merjashourov13@gmail.com>
2025-11-07 06:37:31 +0600

    db connect

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
+
+connectDB();
+dotenv.config();
+
+const app = express();
+
+// middleware
+app.use(cors())
+app.use(express.json());
+
+// Routes
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+
+
+
+const PORT = process.env.PORT || 5001;
+app.listen( PORT, ()=> console.log(`server running on port ${PORT}`));
+
